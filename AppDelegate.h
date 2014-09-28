05813cbf1b448ba43aea50603221633874f1c630
NathanialLarson <nathanial.larson@gmail.com>
2014-09-28 10:46:20 -0600

    test
    test

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coreFoundationSnippets
+//
+//  Created by Nathanial Larson on 9/28/14.
+//  Copyright (c) 2014 Nathanial Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coreFoundationSnippets
+//
+//  Created by Nathanial Larson on 9/28/14.
+//  Copyright (c) 2014 Nathanial Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
