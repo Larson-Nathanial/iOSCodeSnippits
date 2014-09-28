840360e385925bf46e733e57905b3074cb87c6e4
NathanialLarson <nathanial.larson@gmail.com>
2014-09-28 10:40:19 -0600

    Empty header and implementation files
    files to contain code snippets for various core Foundation code snippets

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coreFoundationSnippets
+//
+//  Created by Nathanial Larson on 9/28/14.
+//  Copyright (c) 2014 Nathanial Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  coreFoundationSnippets
+//
+//  Created by Nathanial Larson on 9/28/14.
+//  Copyright (c) 2014 Nathanial Larson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
